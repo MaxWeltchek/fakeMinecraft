5bbcdac1949342c2ef200348511b4f72065875ab
MaxWeltchek <maxweltchek@gmail.com>
2026-02-13 20:57:14 -0800

    added new cube constructor that generates cubes of a given radius at a given center

--- src/Cube.java
+++ src/Cube.java
@@ -11,4 +11,29 @@ public class Cube extends Shapes{
                 {1, 2, 6, 5}
         };
     }
+
+    public Cube(Points center, int radius) {
+        Points[] vertices = new Points[8];
+        int i = 0;
+        for (int x = 0; x < 2; x++) {
+            for (int y = 0; y < 2; y++) {
+                for (int z = 0; z < 2; z++) {
+                    vertices[i++] = new Points(
+                            center.getCoordinates()[0] + (x == 0 ? radius: -radius),
+                            center.getCoordinates()[1] + (y == 0 ? radius: -radius),
+                            center.getCoordinates()[2] + (z == 0 ? radius: -radius)
+                    );
+                }
+            }
+        }
+        super(vertices);
+        faces = new int[][] {
+                {0, 1, 2 ,3},
+                {3, 2, 6, 7},
+                {7, 6, 5, 4},
+                {4, 5, 1, 0},
+                {0, 3, 7, 4},
+                {1, 2, 6, 5}
+        };
+    }
 }
