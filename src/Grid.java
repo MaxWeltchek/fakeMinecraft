ef70a148f9d4fe7fe156ac3a116552c0b77280ba
MaxWeltchek <maxweltchek@gmail.com>
2026-02-24 22:29:47 -0800

    code cleanup removed commented out legacy grid constructor renamed tl to topLeft for clarity

--- src/Grid.java
+++ src/Grid.java
@@ -3,37 +3,7 @@ public class Grid {
     private Points[][] vertices;
     private Points[] oneDimensionArrayVertices;
     private int[][] faces;
-    private Mesh gridMesh;
-
-//    public Grid(int width, int yCoord, int numSquaresPerLine_) {
-//        int step = width/numSquaresPerLine_;
-//        Points[][] vertices = new Points[numSquaresPerLine_ + 1][numSquaresPerLine_ + 1];
-//        for (int i = 0; i < numSquaresPerLine_ + 1; i ++) {
-//            for (int j = 0; j <= numSquaresPerLine_; j++) {
-//                vertices[i][j] = new Points(i * step, yCoord, j* step);
-//            }
-//        }
-//        ArrayList<Points> temp = new ArrayList<>();
-//        for (int i = 0; i <= numSquaresPerLine_; i++) {
-//            for (int j = 0; j <= numSquaresPerLine_; j++) {
-//                temp.add(vertices[i][j]);
-//            }
-//        }
-//        Points[] oneDimensionArrayVertices = new Points[temp.size()];
-//        for (int i = 0; i < temp.size(); i++) {
-//            oneDimensionArrayVertices[i] = temp.get(i);
-//        }
-//
-//
-//        int[][] faces = new int[numSquaresPerLine_ * numSquaresPerLine_][4];
-//        for (int i = 0; i < numSquaresPerLine_; i++) {
-//            for (int j = 0; j < numSquaresPerLine_; j++) {
-//                faces[i + j * numSquaresPerLine_] = new int[]{temp.indexOf(vertices[i][j]), temp.indexOf(vertices[i][j + 1]), temp.indexOf(vertices[i + 1][j + 1]), temp.indexOf(vertices[i + 1][j])};
-//            }
-//        }
-//
-//        gridMesh = new Mesh(oneDimensionArrayVertices, faces);
-//    }
+    private final Mesh gridMesh;
 
     public Grid(int width, int yCord, int numSquaresPerLine_) {
         int numVerticesPerLine = numSquaresPerLine_ + 1;
@@ -49,12 +19,12 @@ public class Grid {
         int[][] faces = new int[numSquaresPerLine_ * numSquaresPerLine_][4];
         for (int i = 0; i < numSquaresPerLine_ - 1; i++) {
             for (int j = 0; j < numSquaresPerLine_; j++) {
-                int tl = i * numVerticesPerLine + j;
+                int topleft = i * numVerticesPerLine + j;
                 faces[i * numSquaresPerLine_ + j] = new int[]{
-                        tl,
-                        tl + 1,
-                        tl + 1 + numVerticesPerLine,
-                        tl + numVerticesPerLine
+                        topleft,
+                        topleft + 1,
+                        topleft + 1 + numVerticesPerLine,
+                        topleft + numVerticesPerLine
                 };            }
         }
 
