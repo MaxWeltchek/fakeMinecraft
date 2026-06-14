91a356505586b56fe747011ee4f17bd1da2bcf7a
MaxWeltchek <maxweltchek@gmail.com>
2026-06-13 21:42:46 -0700

    fix: fixed items not stacking when shift clicked together, and reasonably should have been

--- src/minecraftclone/inventory/Inventory.java
+++ src/minecraftclone/inventory/Inventory.java
@@ -1,5 +1,8 @@
 package minecraftclone.inventory;
 
+import minecraftclone.Main;
+
+import java.awt.event.MouseEvent;
 import java.util.ArrayList;
 
 public class Inventory {
@@ -62,4 +65,189 @@ public class Inventory {
         }
         return allItems;
     }
+
+    public void moveItems(int x, int y, MouseEvent button) {
+        //shift down handling TODO: add auto stacking when shift clicking
+        if (Main.shiftPressed) {
+            int firstAvailable = -1;
+            int nearestCell = findNearestCell(new int[]{x, y});
+
+            //if the square picked is an inventory cell find the first hotbar slot free
+            if (nearestCell <= 26) {
+                for (int i = 35; i >= 27; i--) {
+                    if (canMoveIntoSpot(i, nearestCell)) {
+                        firstAvailable = i;
+                        break;
+                    }
+                }
+            } else if (nearestCell <= 35) { //if the square picked is a hotbar slot, put it in the first (furthest top and left) inventory slot free
+                for (int i = 0; i <= 26; i++) {
+                    if (canMoveIntoSpot(i, nearestCell)) {
+                        firstAvailable = i;
+                        break;
+                    }
+                }
+            } else { //if it's a crafting grid square, try hotbar then inventory with same logic as above
+                //tries hotbar
+                for (int i = 35; i >= 27; i--) {
+                    if (canMoveIntoSpot(i, nearestCell)) {
+                        firstAvailable = i;
+                        break;
+                    }
+                }
+                if (firstAvailable == -1) { //if no free hotbar slots try inventory
+                    for (int i = 0; i <= 26; i++) {
+                        if (canMoveIntoSpot(i, nearestCell)) {
+                            firstAvailable = i;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            //do nothing if no available slots
+            if (firstAvailable == -1) {
+                return;
+            }
+
+            //sets new item in that cell, moves it, then clears original cell
+            //unsure if this is less overhead than checking if nothing would happen in the first place (i.e. firstAvailable == nearestCell)
+            if (this.isOccupied(nearestCell)) {
+                moveItems(nearestCell, firstAvailable);
+//                this.setItemInSlot(firstAvailable, this.getItemInSlot(nearestCell).clone());
+//                this.getItemInSlot(firstAvailable).move(this.getCenterCoords(firstAvailable));
+//                this.clearSlot(nearestCell);
+            }
+            //short circuit instead of giant if case
+        }
+
+        //if not holding anything
+        else if (Main.heldItem == null) {
+            for (Item item : this.getAllItems()) {
+                if (item == null) {
+                    continue;
+                }
+                //if inside the item box
+                if (x > item.getCoords()[0] - Main.itemDimension / 2 && x < item.getCoords()[0] + Main.itemDimension / 2 && y > item.getCoords()[1] - Main.itemDimension / 2 && y < item.getCoords()[1] + Main.itemDimension / 2) {
+                    //if left click
+                    if (findNearestCell(new int[]{x, y}) == 45) {
+                        Main.decrementCraftingSlots();
+                        Main.craftingGridUpdated = true;
+                    } else if (findNearestCell(new int[]{x, y}) >= 36 && findNearestCell(new int[]{x, y}) < 45) {
+                        Main.craftingGridUpdated = true;
+                    }
+                    if (button.getButton() == MouseEvent.BUTTON1) {
+                        //clear the best slot
+                        this.setItemInSlot(findNearestCell(new int[]{x, y}), null);
+                        Main.heldItem = item;
+                        break;
+                    } else if (button.getButton() == MouseEvent.BUTTON3) { //split the stack if right click
+                        Main.heldItem = this.getItemInSlot(findNearestCell(new int[]{x, y})).clone();
+                        this.getItemInSlot(findNearestCell(new int[]{x, y})).setAmount(this.getItemInSlot(findNearestCell(new int[]{x, y})).getAmount() / 2);
+                        Main.heldItem.setAmount((Main.heldItem.getAmount() / 2) + (Main.heldItem.getAmount() %2));
+
+                        if (this.getItemInSlot(findNearestCell(new int[]{x, y})).getAmount() == 0) {
+                            this.clearSlot(findNearestCell(new int[]{x, y}));
+                        }
+                    }
+                }
+            }
+        } else { //if holding something
+            //find the "best point" which is just the cell the item should snap to
+            int bestPoint = findNearestCell(new int[]{x, y});
+
+            //set flag if we update crafting grid to save compute (slow craft checking alg)
+            if (bestPoint >= 36 && bestPoint < 45) {
+                Main.craftingGridUpdated = true;
+            } //or if we are holding something we can't click on crafting output slot
+            else if (bestPoint == 45) {
+                if (this.isOccupied(45) && Main.heldItem.getLore().equals(this.getItemInSlot(45).getLore())) {
+                    Main.heldItem.setAmount(Main.heldItem.getAmount() + this.getItemInSlot(45).getAmount());
+                    Main.decrementCraftingSlots();
+                    Main.craftingGridUpdated = true;
+                }
+                return;
+            }
+            //if that cell is empty then update it with the held item, then clear the held item
+            if (!this.isOccupied(bestPoint)) {
+                this.setItemInSlot(bestPoint, Main.heldItem);
+                Main.heldItem.move(this.getCenterCoords(bestPoint));
+                Main.heldItem = null;
+            }
+            //if the cell is occupied, AND the held item and item occupying the slot are the same, just add the stack sizes (if stackable)
+            else if (this.isOccupied(bestPoint) && (this.getItemInSlot(bestPoint).getLore().equals(Main.heldItem.getLore())) && Main.heldItem.isStackable()) {
+                //if adding the two would make the stack >64, set the slot to 64 and hand to leftover
+                if (Main.heldItem.getAmount() + this.getItemInSlot(bestPoint).getAmount() > 64) {
+                    Main.heldItem.setAmount((this.getItemInSlot(bestPoint).getAmount() + Main.heldItem.getAmount()) - 64);
+                    this.getItemInSlot(bestPoint).setAmount(64);
+                } else {
+                    this.getItemInSlot(bestPoint).setAmount(this.getItemInSlot(bestPoint).getAmount() + Main.heldItem.getAmount());
+                    Main.heldItem = null;
+                }
+            }
+            //finally if its just occupied and not the same (stackable) swap them
+            else if (this.isOccupied(bestPoint) && !this.getItemInSlot(bestPoint).getLore().equals(Main.heldItem.getLore())) {
+                Item temp = Main.heldItem.clone();
+                Main.heldItem = this.getItemInSlot(bestPoint);
+                this.setItemInSlot(bestPoint, temp);
+                this.getItemInSlot(bestPoint).move(this.getCenterCoords(bestPoint));
+            }
+        }
+    }
+
+    private void moveItems(int cell1, int cell2) {
+        if (!isOccupied(cell2)) {
+            getItemInSlot(cell1).move(getCenterCoords(cell2));
+            setItemInSlot(cell2, getItemInSlot(cell1));
+            clearSlot(cell1);
+        } else if (!ItemRegistry.isStackable(getItemInSlot(cell1).getLore())) {
+            getItemInSlot(cell1).move(getCenterCoords(cell2));
+            setItemInSlot(cell2, getItemInSlot(cell1));
+            clearSlot(cell1);
+        } else {
+            int sum = getItemInSlot(cell1).getAmount() + getItemInSlot(cell2).getAmount();
+            if (sum <= 64) {
+                getItemInSlot(cell1).move(getCenterCoords(cell2));
+                setItemInSlot(cell2, getItemInSlot(cell1));
+                getItemInSlot(cell2).setAmount(sum);
+                clearSlot(cell1);
+            } else {
+                int diff = (64 - getItemInSlot(cell2).getAmount());
+                setItemInSlot(cell2, getItemInSlot(cell1));
+                getItemInSlot(cell2).setAmount(64);
+                getItemInSlot(cell1).setAmount(diff);
+            }
+        }
+    }
+
+    private boolean canMoveIntoSpot(int target, int source) {
+        if (!isOccupied(target)) {
+            return true;
+        }
+
+        Item targetItem = getItemInSlot(target);
+        Item sourceItem = getItemInSlot(source);
+
+        return sourceItem != null
+                && targetItem.getLore().equals(sourceItem.getLore())
+                && ItemRegistry.isStackable(sourceItem.getLore());
+    }
+
+    private int findNearestCell(int[] coords) {
+        double bestDistance = Integer.MAX_VALUE;
+        int bestPoint = -1;
+        for (int i = 0; i < this.size(); i++) {
+            double temp = computeSquaredDistance(this.getCenterCoords(i), coords);
+            if (temp < bestDistance) {
+                bestDistance = temp;
+                bestPoint = i;
+            }
+        }
+        return bestPoint;
+    }
+
+    private double computeSquaredDistance(int[] coords1, int[] coords2) {
+        return Math.pow(coords2[0] - coords1[0], 2) + Math.pow(coords2[1] - coords1[1], 2);
+    }
+
 }
